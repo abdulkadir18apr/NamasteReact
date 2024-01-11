259e3f56f993155976f000f5b41b3b8426b60877
abdulkadir18apr <114944605+abdulkadir18apr@users.noreply.github.com>
2024-01-11 11:59:24 +0530

    jsx

--- App.js
+++ App.js
@@ -3,4 +3,8 @@ import  ReactDOM  from "react-dom/client";
 const heading=React.createElement("h1",{id:"heading"},"Namaste React")
 console.log(heading);
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+
+
+const jsxHeading=<h1>Namaste React using Jsx</h1>
+console.log(jsxHeading)
+root.render(jsxHeading)
